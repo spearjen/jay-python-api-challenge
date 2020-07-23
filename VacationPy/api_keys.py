c208969abb4ac7e087cecc09480cb822423b8a14
Jay Spears <j.spears.denver@gmail.com>
2020-07-23 08:06:17 -0600

    first commit

---
+++ VacationPy/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "59b825c60b50901968c395098640d382"
+
+# Google API Key
+g_key = "AIzaSyDa_LHlgs1thrbYkmRLJ3otwFKYdDHZuMw"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,2 @@
+# OpenWeatherMap API Key
+weather_api_key = "59b825c60b50901968c395098640d382"
